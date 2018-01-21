4ab786072e0905550487a1ab6e1bfc8358490fd4
TMD Associates Limited <a.macey@tmd-associates.co.uk>
2018-01-21 19:24:52 +0000

    Added small dataset

--- dpwk4/ui.R
+++ dpwk4/ui.R
@@ -13,16 +13,16 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("AirQuality London Jan 2017/2018"),
+  titlePanel("Air Quality Central London 2017"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
+       sliderInput("Month",
+                   "Month:",
                    min = 1,
                    max = 12,
-                   value = 30)
+                   value = 6)
     ),
     
     # Show a plot of the generated distribution
